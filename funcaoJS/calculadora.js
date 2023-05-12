6777dedc65086eb1633f93fb8e9469a6b4dfbc02
annamatos <anna.matos@sptech.school>
2023-05-12 11:06:25 -0300

    Calculadora.js

---
+++ funcaoJS/calculadora.js
@@ -0,0 +1,41 @@
+function calculoCachorro(){
+    var nome = input_nome.value;
+    var anos = Number(input_anos.value);
+    var porte = Number(input_porte.value);
+   
+     
+    if (porte == 1) {
+        if (anos <= 2 ){
+            var idade = (anos * 12.5)
+        } else {
+            var idade = 23 + ((anos - 2) * 4.5);
+    }}else if (porte == 2){
+        if (anos <= 2 ){
+            var idade = (anos * 10.5)
+        } else {
+            var idade = 19 + ((anos - 2) * 5.7);
+    }} else {
+        if (anos <= 2 ){
+            var idade = (anos * 9)
+        } else {
+            var idade = 16 + ((anos - 2) * 8);
+    }}
+    div_resultado.innerHTML = `O ${nome} tem ${idade} anos humanos.`
+}
+
+function calculoGato(){
+    var nome = input_nomeGato.value;
+    var anos = Number(input_anosGato.value);
+    
+    if (anos <=1){
+        var idade = anos * 15
+    }else if(anos == 2){
+        var idade = 15 + 10
+    } else {
+        var idade = 25 +((anos - 2) * 4)
+    }
+
+
+    div_resultadoGato.innerHTML = `O ${nome} tem ${idade} anos humanos.`
+
+}
