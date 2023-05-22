b2e8f9b8618bd91160f8dd93b2b63cc0647ab8f7
annamatos <anna.matos@sptech.school>
2023-05-21 21:09:15 -0300

    Atulizaçao de todas áreas do site, mudança no CSS, HTML E JS

--- funcaoJS/cadastro.js
+++ funcaoJS/cadastro.js
@@ -1,23 +1,21 @@
-function cadastrar(){
-    var nome= input_nome.value
-    var sobrenome= input_sobrenome.value
-    var genero= input_genero.value
-    var estado= input_estado.value
-    var email= input_email.value
-    var senha= input_senha.value
-    var confirmeSenha= input_confirmeSenha.value
+function cadastrar() {
+    var nome = input_nome.value
+    var sobrenome = input_sobrenome.value
+    var genero = input_genero.value
+    var estado = input_estado.value
+    var email = input_email.value
+    var senha = input_senha.value
+    var confirmeSenha = input_confirmeSenha.value
 
-    if (nome =="" || sobrenome =="" || genero =="" || estado =="")
-    {
+    if (nome == "" || sobrenome == "" || genero == "" || estado == "") {
         alert(`Campo(s) vazio!`)
-    } else if(email =="" || senha =="" || confirmeSenha ==""){
+    } else if (email == "" || senha == "" || confirmeSenha == "") {
         alert(`email e/ou senha não informados`)
-    } 
-    else if (senha != confirmeSenha)
-    {
+    }
+    else if (senha != confirmeSenha) {
         alert(`Senhas diferentes`)
-    }else {
-        href="login.html"
+    } else {
+        href = "login.html"
     }
 
 }
--- funcaoJS/calculadora.js
+++ funcaoJS/calculadora.js
@@ -1,41 +1,56 @@
-function calculoCachorro(){
+function calculoCachorro() {
     var nome = input_nome.value;
     var anos = Number(input_anos.value);
-    var porte = Number(input_porte.value);
-   
-     
+    var selectElement = document.getElementById("meuSelect");
+    var porte = selectElement.value;
+
     if (porte == 1) {
-        if (anos <= 2 ){
+        if (anos <= 2) {
             var idade = (anos * 12.5)
         } else {
             var idade = 23 + ((anos - 2) * 4.5);
-    }}else if (porte == 2){
-        if (anos <= 2 ){
+        }
+    } else if (porte == 2) {
+        if (anos <= 2) {
             var idade = (anos * 10.5)
         } else {
             var idade = 19 + ((anos - 2) * 5.7);
-    }} else {
-        if (anos <= 2 ){
+        }
+    } else {
+        if (anos <= 2) {
             var idade = (anos * 9)
         } else {
             var idade = 16 + ((anos - 2) * 8);
-    }}
+        }
+    }
     div_resultado.innerHTML = `O ${nome} tem ${idade} anos humanos.`
+
+    if (porte == 0) {
+        alert(`Informe o porte do seu cachorro.`)
+        div_resultado.innerHTML = ""
+    } else if (anos <= 0) {
+        alert(`Informe a idade do cachorro.`)
+        div_resultado.innerHTML = ""
+    }
 }
 
-function calculoGato(){
+function calculoGato() {
     var nome = input_nomeGato.value;
     var anos = Number(input_anosGato.value);
-    
-    if (anos <=1){
+
+    if (anos <= 1) {
         var idade = anos * 15
-    }else if(anos == 2){
+    } else if (anos == 2) {
         var idade = 15 + 10
     } else {
-        var idade = 25 +((anos - 2) * 4)
+        var idade = 25 + ((anos - 2) * 4)
     }
-
-
+    
     div_resultadoGato.innerHTML = `O ${nome} tem ${idade} anos humanos.`
+
+    if (anos <= 0) {
+        alert(`Informe a idade do gato.`)
+        div_resultadoGato.innerHTML = ""
+    }
 
 }
